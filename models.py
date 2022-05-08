5e351cd8c73641365e7612bbbb6c49d09e983428
robjoh <rjohanse@ucsc.edu>
2022-05-08 14:13:37 -0700

    Updated some of the DB, still working on all the references and whether we need more fields

--- models.py
+++ models.py
@@ -2,6 +2,7 @@
 This file defines the database models
 """
 
+from pickle import FALSE
 from .common import db, Field, auth
 from pydal.validators import *
 
@@ -23,11 +24,31 @@ def get_time():
 db.define_table(
     'event',
     Field('event_title', requires=IS_NOT_EMPTY()),
-    Field('event_image', "upload"),
+    Field('event_image', 'upload'),
     Field('event_location', requires=IS_NOT_EMPTY()),
     Field('event_description', requires=IS_NOT_EMPTY()),
-    Field('event_attachment', "upload"),
-    Field('user_email', default=get_user_email)
+    Field('event_attachment', 'upload'),
+    Field('user_email', default=get_user_email),
+    Field('pending_invite', 'boolean', default=FALSE),
+)
+
+db.define_table(
+    'profile',
+    Field('prof_first_name', requires=IS_NOT_EMPTY()),
+    Field('prof_last_name', requires=IS_NOT_EMPTY()),
+    Field('description'),
+    Field('notifications' 'reference events'),
+    Field('events', 'reference event'),
+
+db.define_table(
+    'invite',
+    Field('invite_first_name', requires=IS_NOT_EMPTY()),
+    Field('invite_last_name', requires=IS_NOT_EMPTY()),
+    Field('invited', 'reference profile_id'),
+    Field('invited_event_id', 'reference event'),
+)
+
+
 )
 
 db.event.id.readable = db.event.id.writable = False
